c71fd30039a8891619a2cc13d8d29a0883352ccf
HerdianBay <104123929+HerdianBay@users.noreply.github.com>
2023-05-31 14:01:27 +0700

    Delete MD directory

--- MD/app/src/main/java/com/example/capstone/mainpage/ui/home/HomeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.capstone.mainpage.ui.home
-
-import androidx.lifecycle.ViewModel
-
-class HomeViewModel : ViewModel() {
-
-}
--- MD/app/src/main/java/com/example/capstone/model/Feedback.kt
+++
@@ -1,6 +0,0 @@
-package com.example.capstone.model
-
-data class Feedback(
-    val feedback : String? = null,
-    val timestamp : Long? = null
-){}
