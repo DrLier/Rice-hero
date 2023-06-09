4826be4977246dfcb257e1ce13f83196ec8ca0e2
Nathan <reyhan.nathaniel.20@student.it.ittelkom-sby.ac.id>
2023-06-10 02:58:00 +0700

    deploying

--- CC/ml to cc/test.py
+++ CC/ml to cc/test.py
@@ -1,6 +1,6 @@
 import requests
 
-resp = requests.post("http://127.0.0.1:5000/",
+resp = requests.post("https://ricediseaseprediction-dumhlsbi3q-et.a.run.app",
                      files={'file': open('brownspot.jpg', 'rb')})
 
 print(resp.json())
