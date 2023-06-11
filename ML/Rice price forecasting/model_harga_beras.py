6b1fe551b70a0eac426cf97141e3069c6471010d
Shelomita <shelomitapuspadara@gmail.com>
2023-06-11 23:36:16 +0700

    Fix model_harga_beras.py and upload forecast result

--- ML/Rice price forecasting/model_harga_beras.py
+++ ML/Rice price forecasting/model_harga_beras.py
@@ -1,3 +1,4 @@
+# Import Libraries
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -9,8 +10,10 @@ from tensorflow.keras.optimizers import Adam
 
 tf.keras.utils.set_random_seed(627)
 
-data = pd.read_csv("ML/Rice price forecasting/data/converted.csv")
+# Import dataset
+data = pd.read_csv("ML/Rice price forecasting/data/harga_beras_mingguan.csv")
 
+# Define window size and split data
 def windowed_dataset(data, window_size):
     X = []
     y = []
@@ -26,17 +29,19 @@ X, y = windowed_dataset(data, window_size)
 X.shape, y.shape
 
 split_time = int(0.9*(len(X)))
+val_split = split_time + math.ceil((len(X) - split_time)/2)
 
-X_train, y_train = X[:148], y[:148]
-X_val, y_val = X[148:157], y[148:157]
-X_test, y_test = X[157:], y[157:]
+X_train, y_train = X[:split_time], y[:split_time]
+X_val, y_val = X[split_time:val_split], y[split_time:val_split]
+X_test, y_test = X[val_split:], y[val_split:]
 X_train.shape, y_train.shape, X_val.shape, y_val.shape, X_test.shape, y_test.shape
 
+# Define the model
 model = Sequential()
 model.add(InputLayer((12,1)))
-model.add(LSTM(128, activation='relu', return_sequences=True))
+model.add(LSTM(256, activation='relu', return_sequences=True))
 model.add(Dropout(0.3))
-model.add(LSTM(128, activation='relu'))
+model.add(LSTM(512, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(1))
@@ -51,7 +56,9 @@ model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50, callbacks
 from tensorflow.keras.models import load_model
 model = load_model('model/')
 
-harga_beras = data.reshape((-1))
+# Generate the forecast
+data.reset_index(inplace=True)
+harga_beras = harga_beras.reshape((-1))
 
 def predict(time_ahead, model):
   prediction_list = harga_beras[-12:]
@@ -78,7 +85,6 @@ forecast_temp = pd.DataFrame(list(forecast_temp), columns=["forecast","forecast_
 
 forecast_results = pd.DataFrame(data={'date' : forecast_dates, 'forecast' : forecast_temp['forecast']})
 forecast_results.set_index('date', inplace=True)
-forecast_results
 
 def myround(x, base=50):
   return base * round(x/base)
@@ -92,4 +98,4 @@ for index, row in forecast_results.iterrows():
 
 rounded_forecast = pd.DataFrame(data={'date' : round_index, 'rounded forecast' : rounded})
 rounded_forecast.set_index('date', inplace=True)
-rounded_forecast
+print(rounded_forecast)
