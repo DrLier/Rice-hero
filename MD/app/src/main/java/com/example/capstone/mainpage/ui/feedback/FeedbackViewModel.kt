6ce45611ca41e06cda644a3d78884b23ba4778e1
HerdianBay <104123929+HerdianBay@users.noreply.github.com>
2023-06-16 17:01:19 +0700

    Delete MD directory

--- MD/app/src/main/java/com/example/capstone/mainpage/ui/feedback/FeedbackViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.capstone.mainpage.ui.feedback
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class FeedbackViewModel : ViewModel() {
-
-}
--- MD/app/src/main/java/com/example/capstone/model/Weather.kt
+++
@@ -1,7 +0,0 @@
-package com.example.capstone.model
-
-data class Weather(
-    val location : String,
-    val temperature : Int,
-    val description : String
-)
